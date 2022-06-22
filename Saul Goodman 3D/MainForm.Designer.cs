c19d45dea1bba11c42f19145fb6de9bae60f8610
Paweł <zlote0000@gmail.com>
2022-06-22 16:19:43 +0200

    Dodaj pliki projektów.

---
+++ Saul Goodman 3D/MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Saul_Goodman_3D
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timeElapsed = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeSaulGoodman = new System.Windows.Forms.ToolStripMenuItem();
+            this.muteSaulGoodman = new System.Windows.Forms.ToolStripMenuItem();
+            this.videoPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.videoPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timeElapsed,
+            this.toolStripSeparator1,
+            this.closeSaulGoodman,
+            this.muteSaulGoodman});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(367, 76);
+            // 
+            // timeElapsed
+            // 
+            this.timeElapsed.Image = global::Saul_Goodman_3D.Properties.Resources.clock;
+            this.timeElapsed.Name = "timeElapsed";
+            this.timeElapsed.Size = new System.Drawing.Size(366, 22);
+            this.timeElapsed.Text = "Saul goodman has been on your desktop for 5 seconds.";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(363, 6);
+            // 
+            // closeSaulGoodman
+            // 
+            this.closeSaulGoodman.Image = global::Saul_Goodman_3D.Properties.Resources.x;
+            this.closeSaulGoodman.Name = "closeSaulGoodman";
+            this.closeSaulGoodman.Size = new System.Drawing.Size(366, 22);
+            this.closeSaulGoodman.Text = "Close Saul Goodman";
+            this.closeSaulGoodman.Click += new System.EventHandler(this.closeSaulGoodman_Click);
+            // 
+            // muteSaulGoodman
+            // 
+            this.muteSaulGoodman.Image = global::Saul_Goodman_3D.Properties.Resources.mute;
+            this.muteSaulGoodman.Name = "muteSaulGoodman";
+            this.muteSaulGoodman.Size = new System.Drawing.Size(366, 22);
+            this.muteSaulGoodman.Text = "Mute Saul Goodman";
+            this.muteSaulGoodman.Click += new System.EventHandler(this.muteSaulGoodman_Click);
+            // 
+            // videoPlayer
+            // 
+            this.videoPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoPlayer.Enabled = true;
+            this.videoPlayer.Location = new System.Drawing.Point(0, 0);
+            this.videoPlayer.Name = "videoPlayer";
+            this.videoPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("videoPlayer.OcxState")));
+            this.videoPlayer.Size = new System.Drawing.Size(800, 450);
+            this.videoPlayer.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.videoPlayer);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Opacity = 0.5D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Saul Goodman 3D";
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.contextMenuStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.videoPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer videoPlayer;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem timeElapsed;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem closeSaulGoodman;
+        private System.Windows.Forms.ToolStripMenuItem muteSaulGoodman;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Saul Goodman 3D/MainForm.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace Saul_Goodman_3D
+{
+    public partial class MainForm : Form
+    {
+        int iTimeElapsed = 0;
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Saul Goodman");
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            IntPtr progman = W32.FindWindow("Progman", null);
+
+            IntPtr result = IntPtr.Zero;
+
+            W32.SendMessageTimeout(progman, 0x052C, new IntPtr(0), IntPtr.Zero, W32.SendMessageTimeoutFlags.SMTO_NORMAL, 1000, out result);
+
+            IntPtr workerw = IntPtr.Zero;
+
+            W32.EnumWindows(new W32.EnumWindowsProc((tophandle, topparamhandle) =>
+            {
+                IntPtr p = W32.FindWindowEx(tophandle, IntPtr.Zero, "SHELLDLL_DefView", IntPtr.Zero);
+
+                if (p != IntPtr.Zero)
+                {
+                    workerw = W32.FindWindowEx(IntPtr.Zero, tophandle, "WorkerW", IntPtr.Zero);
+                }
+                return true; 
+
+            }), IntPtr.Zero);
+
+            W32.SetParent(Handle, workerw);
+
+            Setup();
+        }
+
+        static void PrintVisibleWindowHandles(IntPtr hwnd, int maxLevel = -1, int level = 0)
+        {
+            bool isVisible = W32.IsWindowVisible(hwnd);
+
+            if (isVisible && (maxLevel == -1 || level <= maxLevel))
+            {
+                StringBuilder className = new StringBuilder(256);
+                W32.GetClassName(hwnd, className, className.Capacity);
+
+                StringBuilder windowTitle = new StringBuilder(256);
+                W32.GetWindowText(hwnd, windowTitle, className.Capacity);
+
+                Console.WriteLine("".PadLeft(level * 2) + "0x{0:X8} \"{1}\" {2}", hwnd.ToInt64(), windowTitle, className);
+
+                level++;
+
+                W32.EnumChildWindows(hwnd, new W32.EnumWindowsProc((childhandle, childparamhandle) =>
+                {
+                    PrintVisibleWindowHandles(childhandle, maxLevel, level);
+                    return true;
+                }), IntPtr.Zero);
+            }
+        }
+
+        void Setup()
+        {
+            
+            Directory.CreateDirectory(path);
+
+            if (File.Exists(Path.Combine(path, "saul-goodman.mp4")) == true)
+            {
+                //do nothing
+            } else
+            {
+                File.WriteAllBytes(Path.Combine(path, "saul-goodman.mp4"), Properties.Resources.saul_goodman);
+            }
+
+            if (File.Exists(Path.Combine(path, "time-elapsed")) == true)
+            {
+                try
+                {
+                    iTimeElapsed = Int32.Parse(File.ReadAllText(Path.Combine(path, "time-elapsed")));
+                } catch(Exception e)
+                {
+                    MessageBox.Show("Caught " + e.GetType() + " when loading \"time-elapsed\" file (Did you modify it?)\n Fix: Go to %appdata% > Saul Goodman > Remove \"time-elapsed\" file.");
+                    videoPlayer.close();
+                    videoPlayer.Dispose();
+                    Close();
+                }
+            }
+            else
+            {
+                File.WriteAllText(Path.Combine(path, "time-elapsed"), "0");
+            }
+
+            videoPlayer.stretchToFit = true;
+            videoPlayer.uiMode = "none";
+            videoPlayer.settings.setMode("loop", true);
+            string filename = Path.Combine(path, "saul-goodman.mp4");
+            videoPlayer.URL = filename;
+
+            notifyIcon.Text = "Saul Goodman";
+            notifyIcon.Icon = this.Icon;
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+            notifyIcon.Visible = true;
+
+
+        }
+        private void closeSaulGoodman_Click(object sender, EventArgs e)
+        {
+            videoPlayer.close();
+            videoPlayer.Dispose();
+            //Process.Start("ms-settings:personalization-background");
+            Close();
+        }
+
+        private void muteSaulGoodman_Click(object sender, EventArgs e)
+        {
+            if (videoPlayer.settings.mute == true)
+            {
+                muteSaulGoodman.Text = "Mute Saul Goodman";
+                videoPlayer.settings.mute = false;
+            } else
+            {
+                muteSaulGoodman.Text = "Unmute Saul Goodman";
+                videoPlayer.settings.mute = true;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            iTimeElapsed++;
+            File.WriteAllText(Path.Combine(path, "time-elapsed"), iTimeElapsed.ToString());
+            timeElapsed.Text = "Saul goodman has been on your desktop for " + iTimeElapsed + " seconds.";
+        }
+    }
+}
